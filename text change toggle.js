74d24996212c3265aec4a6c9e906fe7beaa39348
siilavanta <53584681+siilavanta@users.noreply.github.com>
2020-01-02 10:50:25 +0600

    Create text change toggle.js

---
+++ text change toggle.js
@@ -0,0 +1,9 @@
+	$('.SeeMore2').click(function(){
+		var $this = $(this);
+		$this.toggleClass('SeeMore2');
+		if($this.hasClass('SeeMore2')){
+			$this.text('See More');			
+		} else {
+			$this.text('See Less');
+		}
+	});
